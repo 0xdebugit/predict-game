dead74926b5e45b738c9e692bd30221126ccab8c
Deepak Natanmai <dnatanmai@in.imshealth.com>
2023-02-03 23:13:36 +0530

    removed dark styles

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,8 @@
 export default function NavBar() {
     return(
-        <nav className="p-3 border-gray-200 bg-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <nav className="p-3 border-gray-200 bg-gray-200">
             <div className="container flex flex-wrap items-center justify-center mx-auto">
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Predict Game</span>
+                <span className="self-center text-xl font-semibold whitespace-nowrap">Predict Game</span>
             </div>
         </nav>
     )
